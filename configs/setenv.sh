50b637e1f659de249f772850c9e6e4e8ae429065
Webserver <mail@tomclaus.be>
2024-04-29 15:28:47 +0200

    Settings Tomcat/Liferay Memory

--- configs/setenv.sh
+++ configs/setenv.sh
@@ -1,6 +1,9 @@
 export CATALINA_OPTS="$CATALINA_OPTS -Xms25600m"
 export CATALINA_OPTS="$CATALINA_OPTS -Xmx25600m"
 export CATALINA_OPTS="$CATALINA_OPTS -XX:MaxPermSize=1024m"
+export CATALINA_OPTS="$CATALINA_OPTS -Dfile.encoding=UTF8"
+export CATALINA_OPTS="$CATALINA_OPTS -Djava.net.preferIPv4Stack=true"
+export CATALINA_OPTS="$CATALINA_OPTS -Dorg.apache.catalina.loader.WebappClassLoader.ENABLE_CLEAR_REFERENCES=false"
+export CATALINA_OPTS="$CATALINA_OPTS -Duser.timezone=GMT"
 
-CATALINA_OPTS="$CATALINA_OPTS -Dfile.encoding=UTF8 -Djava.net.preferIPv4Stack=true -Dorg.apache.catalina.loader.WebappClassLoader.ENABLE_CLEAR_REFERENCES=false -Duser.timezone=GMT"
 CATALINA_OPTS="${CATALINA_OPTS} ${LIFERAY_JVM_OPTS}"
